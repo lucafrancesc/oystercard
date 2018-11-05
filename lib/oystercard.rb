726af8eb47e38f252d58f2bbda616a4b3f3fdd74
Luca <eto.lucafrancesco@gmail.com>
2018-11-05 22:12:03 +0000

    Elenventh step - remember (remember) the entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,15 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station
   DEFAULT_BALANCE = 0.00
   CARD_LIMIT = 90.00
   MIN_FARE = 1
 
-  def initialize(balance = DEFAULT_BALANCE, limit = CARD_LIMIT)
+  def initialize(balance = DEFAULT_BALANCE, limit = CARD_LIMIT, entry_station = nil)
     @balance = balance
     @limit = limit
     @in_use = false
+    @entry_station = entry_station
   end
 
   def top_up(amount)
@@ -19,18 +20,24 @@ class Oystercard
   end
 
   def in_journey?
-    @in_use
+    !!entry_station
+    # entry_station.nil? ? false : true
   end
 
-  def touch_in
+  def touch_in(station)
     fail 'Not enough credit' if MIN_FARE > @balance
+    @entry_station = station
     @in_use = true
   end
 
   def touch_out
+    @entry_station = nil
     deduct
     @in_use = false
+    balance
   end
+
+
 
   private
   def full?
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,51 +2,70 @@ require 'oystercard'
 
 describe Oystercard do
 
+  # let(:oyster2) { Oystercard.new(10.00) }
+  let(:oyster) { Oystercard.new }
+  let(:station){ double :station }
   describe '#balance' do
     it 'expects the default balance to be 0' do
-      expect(subject.balance).to eq 0.00
+      expect(oyster.balance).to eq 0.00
     end
   end
 
   describe '#top_up' do
     it 'expects the balance to be updated' do
-      expect{ subject.top_up(10.00) }.to change{ subject.balance }.by 10.00
+      expect{ oyster.top_up(10.00) }.to change{ oyster.balance }.by 10.00
     end
 
     it 'raises an error if the card maximum balance is exceeded' do
       card_limit = Oystercard::CARD_LIMIT
-      subject.top_up(card_limit)
-      expect { subject.top_up(0.01) }.to raise_error "Card limit of #{card_limit} exceeded"
+      oyster.top_up(card_limit)
+      expect { oyster.top_up(0.01) }.to raise_error "Card limit of #{card_limit} exceeded"
     end
 
     it 'raises an error if the top up amount is bigger than the card maximum' do
       card_limit = Oystercard::CARD_LIMIT
-      expect { subject.top_up(card_limit+1) }.to raise_error "Card limit of #{card_limit} exceeded"
+      expect { oyster.top_up(card_limit+1) }.to raise_error "Card limit of #{card_limit} exceeded"
     end
   end
 
   describe '#deduct' do
+
     it 'expects the balance to be updated' do
-      subject.top_up(10.00)
-      subject.touch_in
-      expect{ subject.touch_out }.to change{ subject.balance }.by -(Oystercard::MIN_FARE)
-      expect(subject).not_to be_in_journey
+      oyster.top_up(10.00)
+      oyster.touch_in(station)
+      expect{ oyster.touch_out }.to change{ oyster.balance }.by -(Oystercard::MIN_FARE)
+      expect(oyster).not_to be_in_journey
     end
 
     it 'raises an error if the balance is less than the min fare' do
-      expect { subject.touch_in }.to raise_error 'Not enough credit'
+      expect { oyster.touch_in(station) }.to raise_error 'Not enough credit'
     end
 
     it 'can touch in and expects to be in_journey' do
-      subject.top_up(10.00)
-      subject.touch_in
-      expect(subject).to be_in_journey
+      oyster.top_up(10.00)
+      oyster.touch_in(station)
+      expect(oyster).to be_in_journey
     end
 
     it 'expects not be be in_journey(in use)' do
-      expect(subject).not_to be_in_journey
+      expect(oyster).not_to be_in_journey
     end
 
+    it 'expects oyster to set the station to nil' do
+      # allow(oyster).to receive(:touch_out) { nil }
+      oyster.touch_out
+      expect(oyster.entry_station).to eq nil
+    end
+  end
+
+  describe '#touch_in' do
+
+    it 'expects oyster to remember the entry station' do
+      # allow(oyster).to receive(:touch_in) { station }
+      oyster.top_up(10.00)
+      oyster.touch_in(station)
+      expect(oyster.entry_station).to eq station
+    end
   end
 
 end
