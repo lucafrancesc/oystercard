47248130d410dbea056e45fac605f311e8b61818
Luca <eto.lucafrancesco@gmail.com>
2018-11-05 16:01:35 +0000

    Fifth step - topping up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
   attr_reader :balance
-  DEFAULT_BALANCE = 0
+  DEFAULT_BALANCE = 0.00
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
 
-  
+  def top_up(amount)
+    @balance += amount
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,10 +4,14 @@ describe Oystercard do
 
   describe '#balance' do
     it 'expects the default balance to be 0' do
-      expect(subject.balance).to eq 0
+      expect(subject.balance).to eq 0.00
     end
   end
 
-  
+  describe '#top_up' do
+    it 'expects the balance to be updated' do
+      expect{ subject.top_up(10.00) }.to change{ subject.balance }.by 10.00
+    end
+  end
 
 end
